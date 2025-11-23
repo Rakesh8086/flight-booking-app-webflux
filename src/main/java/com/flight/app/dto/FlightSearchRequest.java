574873e2034d3a0912a4af1062a55c0c11346307
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-24 01:19:59 +0530

    Added flight search request dto for searching flights

---
+++ src/main/java/com/flight/app/dto/FlightSearchRequest.java
@@ -0,0 +1,18 @@
+package com.flight.app.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class FlightSearchRequest {
+    @NotBlank
+    private String fromPlace;
+    
+    @NotBlank
+    private String toPlace;
+    
+    @NotNull
+    private LocalDate journeyDate;
+}
