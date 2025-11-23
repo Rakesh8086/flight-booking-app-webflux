75aad0c43e4d0529b74b7a99b53604c1aba6e8bf
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-23 23:01:19 +0530

    Spring WebFlux project setup and initial dependencies added

---
+++ src/main/java/com/flight/app/TempReactiveProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flight.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempReactiveProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempReactiveProjectApplication.class, args);
+	}
+
+}
