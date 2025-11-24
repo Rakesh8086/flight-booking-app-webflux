1b45d7daf989fa6b39c9ee1d25cc9003bfc84886
Rakesh8086 <rakeshteja7007@gmail.com>
2025-11-24 10:53:55 +0530

    Added DTOs for getting booking and passenger details

---
+++ src/main/java/com/flight/app/dto/BookingRequest.java
@@ -0,0 +1,35 @@
+package com.flight.app.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingRequest {
+
+    @NotBlank
+    private String userName;
+
+    @NotBlank
+    @Email(message = "Email must be a valid format.")
+    private String userEmail;
+    
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number must be 10 digits.")
+    private String mobileNumber; 
+
+    @NotNull
+    @Pattern(regexp = "Veg|NonVeg", message = "Meal option must be 'Veg' or 'NonVeg'.")
+    private String mealOpted; 
+    
+    @Valid // This one checks validation on the list elements on PassengerDTO
+    @NotEmpty(message = "Passenger details are required for booking.")
+    private List<PassengerDTO> passengers;
+    
+}
---
+++ src/main/java/com/flight/app/dto/PassengerDTO.java
@@ -0,0 +1,30 @@
+package com.flight.app.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PassengerDTO {
+
+    @NotBlank
+    private String name;    
+
+    @NotBlank
+    private String gender;  
+
+    @NotNull
+    @Min(value = 0)
+    private Integer age;    
+    
+    @NotBlank
+    private String seatNumber;
+
+}
